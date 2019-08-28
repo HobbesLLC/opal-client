fb034159d42bcf75f436c053b27943bb3cedc16b
Lucas Vocos <lucas.vocos@me.com>
2019-08-27 23:52:15 -0400

    set sidebar text vertically aligned

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -37,21 +37,25 @@ export default class Footer extends React.Component {
   render() {
     let expandedDiv = (
         <aside className='about'>
-          <h1>About</h1>
-          <p>Opal is a free web tool for creating fully customize animated JSON iconsets</p>
-          <p>json files are lightweight text files that (with the help of <a href='https://aescripts.com/bodymovin/' target='_blank' rel="noopener noreferrer">Bodymovin</a> and <a href='https://airbnb.io/lottie/#/' target='_blank' rel="noopener noreferrer">Lottie</a>) can be used to render SVG animations. This assures that each animated icon is vector-based and fully scalable for any website or application.</p>
-          <p>Check out the details on the Lottie Website here or in the readme file included in the download.</p>
-          <p>Created by <a href='https://www.hobbes.work'>Hobbes</a></p>
+          <div className='about-content'>
+            <h1>About</h1>
+            <p>Opal is a free web tool for creating fully customize animated JSON iconsets</p>
+            <p>json files are lightweight text files that (with the help of <a href='https://aescripts.com/bodymovin/' target='_blank' rel="noopener noreferrer">Bodymovin</a> and <a href='https://airbnb.io/lottie/#/' target='_blank' rel="noopener noreferrer">Lottie</a>) can be used to render SVG animations. This assures that each animated icon is vector-based and fully scalable for any website or application.</p>
+            <p>Check out the details on the Lottie Website here or in the readme file included in the download.</p>
+            <p>Created by <a href='https://www.hobbes.work'>Hobbes</a></p>
+          </div>
         </aside>
     )
     if (this.state.expanded) {
       expandedDiv = (
         <aside className='about expanded'>
-          <h1>About</h1>
-          <p>Opal is a free web tool for creating fully customize animated JSON iconsets</p>
-          <p>json files are lightweight text files that (with the help of <a href='https://aescripts.com/bodymovin/' target='_blank' rel="noopener noreferrer">Bodymovin</a> and <a href='https://airbnb.io/lottie/#/' target='_blank' rel="noopener noreferrer">Lottie</a>) can be used to render SVG animations. This assures that each animated icon is vector-based and fully scalable for any website or application.</p>
-          <p>Check out the details on the Lottie Website here or in the readme file included in the download.</p>
-          <p>Created by <a href='https://www.hobbes.work'>Hobbes</a></p>
+          <div className='about-content'>
+            <h1>About</h1>
+            <p>Opal is a free web tool for creating fully customize animated JSON iconsets</p>
+            <p>json files are lightweight text files that (with the help of <a href='https://aescripts.com/bodymovin/' target='_blank' rel="noopener noreferrer">Bodymovin</a> and <a href='https://airbnb.io/lottie/#/' target='_blank' rel="noopener noreferrer">Lottie</a>) can be used to render SVG animations. This assures that each animated icon is vector-based and fully scalable for any website or application.</p>
+            <p>Check out the details on the Lottie Website here or in the readme file included in the download.</p>
+            <p>Created by <a href='https://www.hobbes.work'>Hobbes</a></p>
+          </div>
         </aside>
       )
     }
