78c223a1ff95e4072b012350d0a627205330b286
Lucas Vocos <lucasvocos@gmail.com>
2020-07-02 10:42:29 -0400

    clean up unused state in Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,38 +1,35 @@
-import React from 'react'
-import './Header.scss'
-import Logo from '../Img/Opal_Logo.svg'
-import Hobbes from '../Img/Hobbes.svg'
-import ReactSVG from 'react-svg'
+import React from 'react';
+import './Header.scss';
+import Logo from '../Img/Opal_Logo.svg';
+import Hobbes from '../Img/Hobbes.svg';
+import ReactSVG from 'react-svg';
 
+export default class Header extends React.Component {
+	render() {
+		return (
+			<>
+				<header className="opal-header">
+					<ReactSVG
+						src={Logo}
+						alt="Opal Logo"
+						aria-label="Opal Logo"
+						className="opal-logo"
+					/>
 
-export default class Header extends React.Component{
-  state = {
-
-  }
-
-  render() {
-
-    return (
-      <>
-      <header className='opal-header'>
-
-          <ReactSVG
-            src={Logo}
-            alt='Opal Logo'
-            aria-label="Opal Logo"
-            className='opal-logo' />
-
-          <a href="https://www.hobbes.work"
-              target="_blank"
-              rel="noopener noreferrer">
-            <ReactSVG src={Hobbes}
-              className='hobbes-logo'
-              aria-label="hobbes Logo"
-              alt='Opal Logo'/>
-          </a>
-
-      </header>
-      </>
-    )
-  }
+					<a
+						href="https://www.hobbes.work"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<ReactSVG
+							src={Hobbes}
+							className="hobbes-logo"
+							aria-label="hobbes Logo"
+							alt="Opal Logo"
+						/>
+					</a>
+				</header>
+			</>
+		);
+	}
 }
